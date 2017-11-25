74b7adb38d3964d87ceca749e9fcf439166a4545
Imac <imac@iMac-de-Imac.local>
2017-11-25 13:04:56 +0100

    Falta dar permisos y hacer capturas. Quiza limpiar comentarios.

--- client.py
+++ client.py
@@ -5,26 +5,43 @@ Programa cliente que abre un socket a un servidor
 """
 
 import socket
+import sys
 
 # Cliente UDP simple.
 
+try:
+    METHOD = sys.argv[1].upper()
+    LOGIN_RECEIVER = sys.argv[2].split('@')[0]
+    IP_SERVER = sys.argv[2].split('@')[-1].split(':')[0]
+    PORT = int(sys.argv[2].split(':')[-1])
+
+except IndexError:
+    print ("Usage: python3 client.py method receiver@IP:SIPport")
+    raise SystemExit
+
 # Dirección IP del servidor.
-SERVER = 'localhost'
-PORT = 6001
+
 
 # Contenido que vamos a enviar
-LINE = '¡Hola mundo!'
+LINE = METHOD + ' sip:' + LOGIN_RECEIVER + '@' + IP_SERVER + ' SIP/2.0'
 
 # Creamos el socket, lo configuramos y lo atamos a un servidor/puerto
 with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as my_socket:
     my_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    my_socket.connect((SERVER, PORT))
+    my_socket.connect((IP_SERVER, PORT))
 
     print("Enviando: " + LINE)
     my_socket.send(bytes(LINE, 'utf-8') + b'\r\n')
     data = my_socket.recv(1024)
-
+    expected = ('SIP/2.0 100 Trying\r\n\r\n'
+            'SIP/2.0 180 Ring\r\n\r\n'
+            'SIP/2.0 200 OK\r\n\r\n')
+    if data == expected:
+        METHOD = 'ACK'
+        LINE = METHOD + ' sip:' + LOGIN_RECEIVER + '@' + IP_SERVER + ' SIP/2.0'
+        my_socket.send(bytes(LINE, 'utf-8') + b'\r\n\r\n')
     print('Recibido -- ', data.decode('utf-8'))
     print("Terminando socket...")
+    # my_socket.close()
 
 print("Fin.")
--- server.py
+++ server.py
@@ -5,6 +5,8 @@ Clase (y programa principal) para un servidor de eco en UDP simple
 """
 
 import socketserver
+import sys
+import os
 
 
 class EchoHandler(socketserver.DatagramRequestHandler):
@@ -14,18 +16,39 @@ class EchoHandler(socketserver.DatagramRequestHandler):
 
     def handle(self):
         # Escribe dirección y puerto del cliente (de tupla client_address)
-        self.wfile.write(b"Hemos recibido tu peticion")
-        while 1:
-            # Leyendo línea a línea lo que nos envía el cliente
-            line = self.rfile.read()
-            print("El cliente nos manda " + line.decode('utf-8'))
-
-            # Si no hay más líneas salimos del bucle infinito
-            if not line:
-                break
+        self.wfile.write(b"Hemos recibido tu peticion\r\n\r\n")
+        methods = ['INVITE', 'ACK', 'BYE']
+        line = self.rfile.read().decode('utf-8').split() #probar sin decode
+        if len(line) != 3: #miramos si la peticion tiene la forma correcta
+            self.wfile.write(b'SIP/2.0 400 Bad Request\r\n\r\n')
+        elif line[0] not in methods:
+            self.wfile.write(b'SIP/2.0 405 Method Not Allowed\r\n\r\n')
+        else:
+            if line[0] == 'INVITE':
+                expected = ('SIP/2.0 100 Trying\r\n\r\n'
+                            'SIP/2.0 180 Ringing\r\n\r\n'
+                            'SIP/2.0 200 OK\r\n\r\n')
+                self.wfile.write(bytes(expected, 'utf-8'))
+            elif line[0] == 'ACK':
+                os.system('./mp32rtp -i 127.0.0.1 -p 23032 < ' +
+                                audio_file)
+            elif line[0] == 'BYE':
+                self.wfile.write(b'SIP/2.0 200 OK\r\n\r\n')
 
 if __name__ == "__main__":
     # Creamos servidor de eco y escuchamos
-    serv = socketserver.UDPServer(('', 6001), EchoHandler)
-    print("Lanzando servidor UDP de eco...")
-    serv.serve_forever()
+    try:
+        serv = socketserver.UDPServer((sys.argv[1],
+                                       int(sys.argv[2])), EchoHandler)
+        audio_file = sys.argv[3]
+        if not os.path.isfile(audio_file):
+            print ("This file does not exist: ' + audio_file")
+            raise SystemExit
+        print("Listening...")
+    except IndexError:
+        print ("Usage: python3 server.py IP port audio_file")
+        raise SystemExit
+    try:
+        serv.serve_forever()
+    except KeyboardInterrupt:
+        print("\nFinalizado servidor")
